ec7c60b21e60aaff9bedd1e4eb3c6439acaebcec
Joseba <joseba.merino.pina@gmail.com>
2020-01-17 12:59:16 +0100

    Componentes nuevos

---
+++ src/app/componentes/boton/boton.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-boton',
+  templateUrl: './boton.component.html',
+  styleUrls: ['./boton.component.scss']
+})
+export class BotonComponent implements OnInit {
+
+  numero: number;
+  incremento: number;
+
+  constructor() { 
+    this.numero = 0;
+    this.incremento = 1;
+  }
+
+  ngOnInit() {
+  }
+
+  pulsado() {
+    this.numero += this.incremento;
+    if(this.numero == 0) {
+      this.incremento = 1;
+    }
+    if(this.numero >= 10) {
+      this.incremento = -1;
+    }
+  }
+
+}
