b17eac03e164a7ea9cbdb28d6650cb50e2d12e9c
Joseba <joseba.merino.pina@gmail.com>
2020-01-17 09:43:19 +0100

    app angular inicio

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
 
+// componenete principal que se carga al arrancar la app
+
+
+/*
+selector:   nombre de la etiqueta para inyectar el componente en HTML
+templateURL:  Vista del HTML
+styleUrls:  Estilo para la vista formato SCSS
+*/
 @Component({
-  selector: 'app-root',
+  selector: 'app-root', 
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  // variable
   title = 'my-app';
 }
