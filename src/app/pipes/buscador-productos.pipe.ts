399e8b85f2426caa6492fc8f349d12c53c3b68ce
Joseba <joseba.merino.pina@gmail.com>
2020-02-06 13:48:00 +0100

    Añadida busqueda

--- src/app/pipes/buscador-productos.pipe.ts
+++ src/app/pipes/buscador-productos.pipe.ts
@@ -1,12 +1,33 @@
 import { Pipe, PipeTransform } from '@angular/core';
+import { stringify } from 'querystring';
 
 @Pipe({
   name: 'buscadorProductos'
 })
 export class BuscadorProductosPipe implements PipeTransform {
 
-  transform(value: any, ...args: any[]): any {
-    return null;
+  transform(recetas: Array<any>, criterioBusqueda: string, sinGluten: boolean): any {
+    if (!criterioBusqueda) {
+      criterioBusqueda = '';
+    }
+    let resultado = new Array<any>();
+
+    criterioBusqueda = criterioBusqueda.toUpperCase();
+
+    if(sinGluten) {
+      recetas = recetas.filter((el)=> el.isGlutenFree);
+    }
+
+    resultado = (recetas.filter((el) => {
+      let valido = false;
+      valido = valido || el.nombre.toUpperCase().includes(criterioBusqueda);
+      valido = valido || el.cocinero.toUpperCase().includes(criterioBusqueda);
+      valido = valido || el.ingredientes.some(el => el.toUpperCase().includes(criterioBusqueda));
+      return valido;
+    }));
+
+
+    return resultado;
   }
 
 }
