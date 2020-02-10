8b5667f804acce5dcc9116c2adc5d25186258a11
Joseba <joseba.merino.pina@gmail.com>
2020-02-10 11:53:36 +0100

    Crear tareas

--- src/app/paginas/tareas/tareas.component.ts
+++ src/app/paginas/tareas/tareas.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Tarea } from 'src/app/model/tarea';
 import { TareasService } from 'src/app/services/tareas.service';
+import { ThrowStmt } from '@angular/compiler';
 
 @Component({
   selector: 'app-tareas',
@@ -10,22 +11,76 @@ import { TareasService } from 'src/app/services/tareas.service';
 export class TareasComponent implements OnInit {
 
   tareas: Array<Tarea>;
+  nuevaTarea: string;
 
   constructor(private servicioTarea: TareasService) {
     console.trace('TareasComponent constructor');
     this.tareas = []; // Inicializar el array
+    this.nuevaTarea = '';
   } // constructor
 
   ngOnInit() {
     console.trace('TareasComponent ngOnInit');
 
     // llamar al service para obtener tareas
+    this.cargarTareas();
+  } // ngOnInit
+  editarEstado(tarea: Tarea) {
+    console.debug('Click %o', tarea);
+    tarea.completada = !tarea.completada;
+
+    this.servicioTarea.modificar(tarea).subscribe(
+      () => {
+        this.cargarTareas();
+      }
+    );
+  }// editarEstado
+
+  /**
+   * Llama al servicio para cargar todas las tareas. Nos va ha servir para refrescar la lista
+   */
+  private cargarTareas(): void {
+    console.trace('cargarTareas');
     this.servicioTarea.listar().subscribe(
       datos => {
-        console.debug('Datos recibidos: %o', datos);
+        console.debug('Datos recibidos:');
+        console.table(datos);
         this.tareas = datos;
       }
     );
-  } // ngOnInit
+  }// cargarTareas
+
+  borrarTarea(tarea: Tarea) {
+    console.debug(`Borrar tarea: %o`, tarea);
+
+    const borrar = confirm('¿Desea borrar la tarea?');
+
+    if(borrar){
+      this.servicioTarea.eliminar(tarea.id).subscribe(
+        () => {
+          console.trace('Borrada tarea');
+          this.cargarTareas();
+        }
+      );
+    } else {
+      console.trace('no se ha querido borrar la tarea');
+    }
+  } // borrarTarea
+
+  crearTarea(nuevaTarea) {
+
+    console.debug('Crear nueva tarea: %s', nuevaTarea);
+    const tarea: Tarea = new Tarea();
+    tarea.titulo = nuevaTarea;
+    this.servicioTarea.crear(tarea).subscribe(
+      dato => {
+        console.trace('Tarea creada %o', dato);
+        this.cargarTareas();
+        this.nuevaTarea = '';
+      }
+    );
+  }// crearTarea
+
+
 
 }
--- src/app/services/tareas.service.ts
+++ src/app/services/tareas.service.ts
@@ -14,21 +14,27 @@ export class TareasService implements ITareasService {
     console.trace('TareasService constructor');
   } // constructor
 
-  listar(): Observable<any> {
+  listar(): Observable<Tarea[]> {
     const url = 'http://localhost:3000/tareas';
     console.debug(`GET ${url}`);
-    return this.http.get(url);
+    return this.http.get<Tarea[]>(url);
   }
   detalle(id: number): Observable<Tarea> {
     throw new Error("Method not implemented.");
   }
   crear(tarea: Tarea): Observable<Tarea> {
-    throw new Error("Method not implemented.");
+    const url = `http://localhost:3000/tareas`;
+    console.debug('POST %s %o', url, tarea);
+    return this.http.post<Tarea>(url, tarea);
   }
   modificar(tarea: Tarea): Observable<Tarea> {
-    throw new Error("Method not implemented.");
+    const url = `http://localhost:3000/tareas/${tarea.id}`;
+    console.debug(`PUT %s tarea %o`, url, tarea);
+    return this.http.put<Tarea>(url, tarea);
   }
   eliminar(id: number): Observable<Tarea> {
-    throw new Error("Method not implemented.");
+    const url = `http://localhost:3000/tareas/${id}`;
+    console.debug(`DELETE ${url}`);
+    return this.http.delete<Tarea>(url);
   }
 } // TareasService
